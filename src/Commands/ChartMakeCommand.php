8caa0e7596959d32e4818060f675418f531d9786
matheusmarnt <matheus.marnt@gmail.com>
2026-05-02 11:25:11 -0300

    feat: implement make:chart artisan command

---
+++ src/Commands/ChartMakeCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Matheusmarnt\LiveCharts\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+class ChartMakeCommand extends GeneratorCommand
+{
+    protected $name = 'make:chart';
+
+    protected $description = 'Create a new chart class';
+
+    protected $type = 'Chart';
+
+    protected function getStub(): string
+    {
+        return __DIR__ . '/../../resources/stubs/chart.php.stub';
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace . '\Charts';
+    }
+
+    protected function replacePlaceholders($stub, $name): string
+    {
+        $stub = str_replace(
+            ['{{ type }}', '{{ engine }}'],
+            [$this->option('type'), $this->option('engine') ?? config('livecharts.engine', 'apexcharts')],
+            $stub
+        );
+
+        return $stub;
+    }
+
+    protected function buildClass($name): string
+    {
+        $stub = parent::buildClass($name);
+
+        return $this->replacePlaceholders($stub, $name);
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['type', 't', InputOption::VALUE_OPTIONAL, 'The type of the chart', 'line'],
+            ['engine', 'e', InputOption::VALUE_OPTIONAL, 'The engine of the chart'],
+        ];
+    }
+}
