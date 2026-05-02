604bb151af7f0d30c180281dd99e7eeaf2553450
matheusmarnt <105521310+matheusmarnt@users.noreply.github.com>
2026-05-02 13:54:33 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Matheusmarnt\LiveCharts\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Matheusmarnt\LiveCharts\LiveChartsServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
